3deb3d319d00a77801d6e953546b252044411f73
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2024-09-25 14:01:24 -0600

    setting up a jsx file for the login page

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+
+// Route to serve the HTML file
 app.get('/', (req, res) => {
-  res.send('Welcome to the Shoe Store API');
+  res.sendFile(path.join(__dirname, '../Frontend/Login.jsx'));
 });
+
+
+//app.get('/', (req, res) => {
+  
+//});
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/Login.html'));
+});
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
